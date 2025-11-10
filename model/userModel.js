c63959e132bdcbe0231424ac062ba2b51f55cbfa
EbrahimShoeib1999 <mrboo9779@gmail.com>
2025-11-10 14:59:11 +0200

    Add phone field to user model with validation

--- model/userModel.js
+++ model/userModel.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
         message: "Invalid email format",
       },
     },
-    c: {
+    phone: {
       type: String,
       required: [true, "phone required"],
       unique: true,
