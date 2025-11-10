52991f2701793d9a867ed8e99935b745d3c3ff10
EbrahimShoeib <ibrahimshoeib255@gmail.com>
2025-11-10 15:33:49 +0200

    fix: craching server

--- controllers/authService.js
+++ controllers/authService.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
-const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const User = require("../model/userModel");
 const ApiError = require("../utils/apiError");
 const createToken = require("../utils/createToken");
 
@@ -128,3 +129,62 @@ exports.resetPassword = asyncHandler(async (req, res, next) => {
 
   res.status(200).json({ data: user, token });
 });
+
+exports.protect = asyncHandler(async (req, res, next) => {
+  // 1) Check if token exist, if exist get
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+  if (!token) {
+    return next(new ApiError("You are not login, Please login to get access this route", 401));
+  }
+
+  // 2) Verify token (no change happens, expired token)
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+  // 3) Check if user exists
+  const currentUser = await User.findById(decoded.userId);
+  if (!currentUser) {
+    return next(
+      new ApiError(
+        "The user that belong to this token does no longer exist",
+        401
+      )
+    );
+  }
+
+  // 4) Check if user change his password after token created
+  if (currentUser.passwordChangedAt) {
+    const passswordChangedTimestamp = parseInt(
+      currentUser.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    // Password changed after token created (Error)
+    if (passswordChangedTimestamp > decoded.iat) {
+      return next(
+        new ApiError(
+          "User recently changed his password. please login again..",
+          401
+        )
+      );
+    }
+  }
+
+  req.user = currentUser;
+  next();
+});
+
+exports.allowedTo = (...roles) =>
+  asyncHandler(async (req, res, next) => {
+    // roles ==> ['admin', 'manager']
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ApiError("You are not allowed to perform this action", 403)
+      );
+    }
+    next();
+  });
