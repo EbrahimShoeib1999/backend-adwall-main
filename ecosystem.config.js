b5f4af9201e93027a871fd9d53d5af7789935baa
EbrahimShoeib <ibrahimshoeib255@gmail.com>
2025-11-16 16:45:38 +0200

    fix: port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       max_memory_restart: "1G",
       env: {
         NODE_ENV: "development",
-      PORT: 3000,
+      PORT: 8000,
               FRONTEND_URL: "https://www.adwallpro.com",
         BASE_URL: "https://www.adwallpro.com",
 
