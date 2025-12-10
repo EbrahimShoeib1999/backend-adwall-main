f36d797cf7914bc2299728f8bb62d7d78767c876
EbrahimShoeib <ibrahimshoeib255@gmail.com>
2025-12-10 16:27:07 +0200

    fix : passport

--- config/passport.js
+++ config/passport.js
@@ -9,6 +9,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.GOOGLE_CALLBACK_URL || `${process.env.BASE_URL}/api/v1/auth/google/callback`,
+      scope: ['profile', 'email'], // Force scope here to avoid invalid_request error
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
